fbe565ec465bca336df121145aea6e7d2575ccf2
Ricardo González Fabelo <rgonzalez@nartexsoft.com>
2022-10-04 00:45:18 +0200

    convertir en WPF

---
+++ GastosRYC/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using GastosRYCLib.Manager;
+using GastosRYCLib.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GastosRYC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private RYCContext? rycContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            loadContext();
+        }
+
+        private void loadContext()
+        {
+            rycContext = new RYCContext();
+            rycContext.categories?.Load();
+            rycContext.persons?.Load();
+            rycContext.accountsTypes?.Load();
+            rycContext.accounts?.Load();
+            rycContext.transactions?.Load();
+        }
+
+        private void frmInicio_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach(AccountsTypes at in rycContext.accountsTypes)
+            {
+                lvCuentas.Items.Add(at);
+             
+                lvCuentas.DisplayMemberPath = "description";
+            }
+        }
+    }
+}
---
+++ GastosRYCLib/Manager/RYCContextFactory.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GastosRYCLib.Manager
+{
+    public class RYCContextFactory
+    {
+        private static volatile RYCContextFactory _dbContextFactory;
+        private static readonly object SyncRoot = new Object();
+        public RYCContext Context;
+
+
+        public static RYCContextFactory Instance
+        {
+            get
+            {
+                if (_dbContextFactory == null)
+                {
+                    //lock (SyncRoot)
+                    //{
+                        if (_dbContextFactory == null)
+                            _dbContextFactory = new RYCContextFactory();
+                    //}
+                }
+                return _dbContextFactory;
+            }
+        }
+
+        public RYCContext GetOrCreateContext()
+        {
+            if (this.Context == null)
+            {
+                this.Context = new RYCContext();
+                this.Context.accountsTypes?.Load();
+                this.Context.categories?.Load();
+                this.Context.accounts?.Load();
+                this.Context.persons?.Load();
+                this.Context.transactions?.Load();
+            }
+
+            return Context;
+        }
+    }
+}
