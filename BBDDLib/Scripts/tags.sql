1824b455441b34490c261b19ad4aa532372782bf
Ricardo González Fabelo <rgonzalez@nartexsoft.com>
2022-11-19 14:02:44 +0100

    script y bbdd

---
+++ BBDDLib/Scripts/tags.sql
@@ -0,0 +1,7 @@
+﻿
+Insert into tags (description) values ('Datos');
+Insert into tags (description) values ('Berlingo');
+Insert into tags (description) values ('Corsa');
+Insert into tags (description) values ('Edif El Alfar');
+Insert into tags (description) values ('Aygo');
+Insert into tags (description) values ('Kona');
