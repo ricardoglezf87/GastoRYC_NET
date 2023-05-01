88542c836389feb21dacc96db06e9980655e33d8
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2023-05-01 07:38:16 +0200

    fix error

--- GastosRYC/Views/Transactions/frmTransaction.xaml.cs
+++ GastosRYC/Views/Transactions/frmTransaction.xaml.cs
@@ -280,6 +280,12 @@ namespace GastosRYC.Views
                 valid = false;
             }
 
+            if(transaction.tranferSplitid != null)
+            {
+                errorMessage += "- No se puede editar una transferencia proveniente de un split\n";
+                valid = false;
+            }
+
             if (errorMessage != "")
             {
                 errorMessage = "Tiene que rellenar los siguiente campos para continuar:\n" + errorMessage.TrimEnd('\n');
