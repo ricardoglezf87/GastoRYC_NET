8761c4688b2a14313ed9dd4423a0269773b5ecb6
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2023-05-04 00:18:53 +0200

    add fields

--- BBDDLib/Models/Transactions.cs
+++ BBDDLib/Models/Transactions.cs
@@ -7,39 +7,24 @@ namespace BBDDLib.Models
     public class Transactions
     {
         public virtual int id { set; get; }
-
         public virtual DateTime? date { set; get; }
-
         public virtual int? accountid { set; get; }
-
         public virtual Accounts? account { set; get; }
-
         public virtual int? personid { set; get; }
-
         public virtual Persons? person { set; get; }
-
         public virtual int? tagid { set; get; }
-
         public virtual Tags? tag { set; get; }
-
         public virtual int? categoryid { set; get; }
-
         public virtual Categories? category { set; get; }
-
         public virtual Decimal? amountIn { set; get; }
-
         public virtual Decimal? amountOut { set; get; }
-
         public virtual int? tranferid { set; get; }
-
         public virtual int? tranferSplitid { set; get; }
-
         public virtual String? memo { set; get; }
-
         public virtual int? transactionStatusid { set; get; }
-
         public virtual TransactionsStatus? transactionStatus { set; get; }
-
+        public virtual int? investmentProductsid { set; get; }
+        public virtual InvestmentProducts? investmentProducts { set; get; }
         public virtual List<Splits>? splits { set; get; }
 
         [NotMapped]
