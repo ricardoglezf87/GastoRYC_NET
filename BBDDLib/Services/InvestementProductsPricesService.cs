f09db72cb375a6a8a54376e5612b688027cc3f3e
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2023-05-08 08:04:34 +0200

    create feature get prices online

--- BBDDLib/Services/InvestementProductsPricesService.cs
+++ BBDDLib/Services/InvestementProductsPricesService.cs
@@ -7,6 +7,7 @@ using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace GastosRYC.BBDDLib.Services
 {
@@ -22,9 +23,15 @@ namespace GastosRYC.BBDDLib.Services
             return RYCContextService.getInstance().BBDD.investmentProductsPrices?.FirstOrDefault(x => id.Equals(x.id));
         }
 
-        public void update(InvestmentProductsPrices investmentProductstags)
+        public bool exists(int? investmentProductId, DateTime? date)
         {
-            RYCContextService.getInstance().BBDD.Update(investmentProductstags);
+            return RYCContextService.getInstance().BBDD.investmentProductsPrices?
+                .Any(x => investmentProductId.Equals(x.investmentProductsid) && date.Equals(x.date)) ?? false;
+        }
+
+        public void update(InvestmentProductsPrices investmentProducts)
+        {
+            RYCContextService.getInstance().BBDD.Update(investmentProducts);
             RYCContextService.getInstance().BBDD.SaveChanges();
         }
 
@@ -41,34 +48,60 @@ namespace GastosRYC.BBDDLib.Services
                 if (investmentProducts == null || String.IsNullOrWhiteSpace(investmentProducts.url))
                     return;
 
-                var httpClient = new HttpClient();
-                httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
-                var response = await httpClient.GetAsync(investmentProducts.url);
-                var html = await response.Content.ReadAsStringAsync();
+                List<InvestmentProductsPrices> lproductsPrices = new();
 
-                var htmlDocument = new HtmlDocument();
-                htmlDocument.LoadHtml(html);
-
-                var tableElement = htmlDocument.DocumentNode.SelectSingleNode("//table[@id='curr_table']");
-
-                var rows = tableElement.SelectNodes("tbody/tr");
-                foreach (var row in rows)
+                if (investmentProducts.url.Contains("investing.com"))
                 {
-                    var cells = row.SelectNodes("td");
-                    var date = cells[0].InnerText;
-                    var price = cells[1].InnerText;
-                    InvestmentProductsPrices productsPrices = new InvestmentProductsPrices();
-                    productsPrices.investmentProductsid = investmentProducts.id;
-                    productsPrices.investmentProducts = investmentProducts;
-                    productsPrices.date = DateTime.ParseExact(date, "dd.MM.yyyy", CultureInfo.InvariantCulture);
-                    productsPrices.prices = Decimal.Parse(price);
-                    update(productsPrices);
+                    lproductsPrices = await getPricesOnlineInvesting(investmentProducts);
                 }
+
+                //TODO: insertar las compras y ventas hechas
+                
+                foreach (InvestmentProductsPrices productsPrices in lproductsPrices)
+                {
+                    if(!exists(productsPrices.investmentProductsid,productsPrices.date))
+                    {
+                        RYCContextService.getInstance().BBDD.Update(productsPrices);
+                    }
+                }
+
+                await RYCContextService.getInstance().BBDD.SaveChangesAsync();
             }
             catch (Exception e)
             {
                 throw;
             }
+        }       
+
+        private async Task<List<InvestmentProductsPrices>> getPricesOnlineInvesting(InvestmentProducts investmentProducts)
+        {            
+            List<InvestmentProductsPrices> lproductsPrices = new();
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
+            var response = await httpClient.GetAsync(investmentProducts.url);
+            var html = await response.Content.ReadAsStringAsync();
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var tableElement = htmlDocument.DocumentNode.SelectSingleNode("//table[@id='curr_table']");
+
+            var rows = tableElement.SelectNodes("tbody/tr");
+            foreach (var row in rows)
+            {
+                var cells = row.SelectNodes("td");
+                var date = cells[0].InnerText;
+                var price = cells[1].InnerText;
+                InvestmentProductsPrices productsPrices = new InvestmentProductsPrices();
+                productsPrices.investmentProductsid = investmentProducts.id;
+                productsPrices.investmentProducts = investmentProducts;
+                productsPrices.date = DateTime.ParseExact(date, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                productsPrices.prices = Decimal.Parse(price);
+                lproductsPrices.Add(productsPrices);
+            }
+
+            return lproductsPrices;
         }
 
     }
