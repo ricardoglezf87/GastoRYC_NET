d7d06cc6b16f5b069df700676033d4f5f2fbf354
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2023-10-29 09:34:40 +0100

    GARCA-4.0 Creación de primer modelo pruebas

--- GARCA_WEBPY/GARCA/views.py
+++ GARCA_WEBPY/GARCA/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from GARCA.models import Accounts
+
 # Create your views here.
 
 def index(request):
+    item = Accounts(description="Efectivo")
+    item.save()
     return HttpResponse("Hello, Django!")
