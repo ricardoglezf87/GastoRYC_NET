aea4a4448e7dabf774306d094f4d8e03b08fa0b6
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2023-11-26 04:35:04 +0100

    GARCA-4.0 Añadir Scritps provisionales

--- DesktopApp/GARCA_DATA/Migrations/Migration_202322142214.cs
+++ DesktopApp/GARCA_DATA/Migrations/Migration_202322142214.cs
@@ -3,13 +3,30 @@ using static GARCA.Data.IOC.DependencyConfig;
 
 namespace GARCA.Data.Migrations
 {
-    public class Migration202322142214
+    public class Migration_202322142214
     {
         public async Task Do()
         {
-            await iRycContextService.getConnection().ExecuteAsync("ALTER TABLE dateCalendar RENAME COLUMN date TO id;");
+            await iRycContextService.getConnection().ExecuteAsync(@"
 
-            await iRycContextService.getConnection().ExecuteAsync("INSERT INTO MigrationsHistory(MigrationId, ProductVersion) VALUES('Migration_202322142214', '4.0');");
+                    DROP TABLE dateCalendar;
+
+                    CREATE TABLE dateCalendar (
+                        ""id"" INTEGER PRIMARY KEY,
+                        ""day"" INTEGER,
+                        ""month"" INTEGER,
+                        ""year"" INTEGER,
+                        ""date"" TEXT
+                    ); 
+
+                ");
+
+            await iRycContextService.getConnection().ExecuteAsync(@"
+
+                INSERT INTO MigrationsHistory(MigrationId, ProductVersion) VALUES('Migration_202322142214', '4.0');
+
+                ");
+
         }
     }
 }
--- DesktopApp/GARCA_DATA/Migrations/ScriptsProvisionales.sql
+++ DesktopApp/GARCA_DATA/Migrations/ScriptsProvisionales.sql
@@ -1,11 +1,1 @@
-﻿ALTER TABLE dateCalendar ADD date TEXT;
-
-DROP TABLE dateCalendar;
-
-CREATE TABLE dateCalendar (
-    "id" INTEGER PRIMARY KEY,
-    "day" INTEGER,
-    "month" INTEGER,
-    "year" INTEGER,
-    "date" TEXT
-);
+﻿
