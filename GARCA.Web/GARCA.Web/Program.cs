8e4fb9bb9138deaa7248f00449aa0cceaa97a681
Ricardo Gonzalez Fabelo <ricardoglezf@gmail.com>
2024-03-24 01:53:53 +0100

    GARCA-5.0 Crear proyecto front

---
+++ GARCA.Web/GARCA.Web/Program.cs
@@ -0,0 +1,30 @@
+using GARCA.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(GARCA.Web.Client._Imports).Assembly);
+
+app.Run();
