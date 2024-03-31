269ebe293dcea119279a934518691faddacb73ed
Ricardo Gonzalez Fabelo <ricardoglezf@gmail.com>
2024-03-31 01:04:21 +0100

    GARCA-5.0 Añadir pagina not found

--- GARCA.Web/GARCA.Web/Program.cs
+++ GARCA.Web/GARCA.Web/Program.cs
@@ -9,17 +9,20 @@ builder.Services.AddRadzenComponents();
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //The default HSTS value is 30 days.You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+app.UseStatusCodePagesWithRedirects("/StatusCode/{0}");
 app.UseHttpsRedirection();
 app.MapControllers();
 app.UseStaticFiles();
 app.UseAntiforgery();
 app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
 app.Run();
