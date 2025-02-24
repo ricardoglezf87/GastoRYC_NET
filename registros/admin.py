afb58a6232250c59587a35eb85a8d88c2a1d358d
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2025-02-24 21:37:46 +0000

    Añadir líneas de apuntes

--- registros/admin.py
+++ registros/admin.py
@@ -1,14 +1,29 @@
 from django.contrib import admin
 from .models import Account, Entry, Transaction
 from mptt.admin import MPTTModelAdmin
+from django import forms
 
 class AccountAdmin(MPTTModelAdmin):
     list_display = ('name', 'parent')
     mptt_level_indent = 20
 
+class TransactionInline(admin.TabularInline):
+    model = Transaction
+    extra = 1
+
+class EntryAdminForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = '__all__'
+        widgets = {
+            'description': forms.TextInput(attrs={'size': 80}),
+        }
+
 class EntryAdmin(admin.ModelAdmin):
+    form = EntryAdminForm
     list_display = ('date', 'description')
     search_fields = ('description',)
+    inlines = [TransactionInline]
 
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ('entry', 'account', 'debit', 'credit')
