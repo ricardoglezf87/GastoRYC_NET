4b479d753a610bea3df1bc67e7932d0c192cf988
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2025-02-24 21:31:43 +0000

    Creación de modelo asiento y apuntes

--- registros/admin.py
+++ registros/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
-from .models import Cuenta, Categoria
+from .models import Account, Entry, Transaction
 from mptt.admin import MPTTModelAdmin
 
-class CuentaAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'saldo')
-    search_fields = ('nombre',)
-
-class CategoriaAdmin(MPTTModelAdmin):
-    list_display = ('nombre', 'parent')
+class AccountAdmin(MPTTModelAdmin):
+    list_display = ('name', 'parent')
     mptt_level_indent = 20
 
-admin.site.register(Cuenta, CuentaAdmin)
-admin.site.register(Categoria, CategoriaAdmin)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('date', 'description')
+    search_fields = ('description',)
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('entry', 'account', 'debit', 'credit')
+    search_fields = ('account__name',)
+
+admin.site.register(Account, AccountAdmin)
+admin.site.register(Entry, EntryAdmin)
+admin.site.register(Transaction, TransactionAdmin)
--- registros/models.py
+++ registros/models.py
@@ -1,22 +1,32 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
-#CUENTA
-class Cuenta(models.Model):
-    nombre = models.CharField(max_length=100)
-    saldo = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+# ACCOUNT
+class Account(MPTTModel):
+    name = models.CharField(max_length=100)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='account_parent')
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
 
     def __str__(self):
-        return self.nombre
+        return self.name
 
-
-#CATEGORIA
-class Categoria(MPTTModel):
-    nombre = models.CharField(max_length=100)
-    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategorias')
-
-    class MOPTTMeta:
-        order_insertion_by = ['nombre']
+# ENTRY
+class Entry(models.Model):
+    date = models.DateField()
+    description = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.nombre
+        return f"{self.date} - {self.description}"
+
+# TRANSACTION
+class Transaction(models.Model):
+    entry = models.ForeignKey(Entry, on_delete=models.CASCADE, related_name='transactions')
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    debit = models.DecimalField(max_digits=10, decimal_places=2)
+    credit = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.account.name} - Debit: {self.debit}, Credit: {self.credit}"
+
