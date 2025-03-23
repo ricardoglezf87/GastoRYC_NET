7b83c4a54db485036259379bc0a13156abccc0ad
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2025-03-23 23:47:34 +0000

    Crear importador transacciones de ing

--- registros/forms.py
+++ registros/forms.py
@@ -17,4 +17,14 @@ class TransactionForm(forms.ModelForm):
         model = Transaction
         fields = ['account', 'debit', 'credit']
 
+class BankImportForm(forms.Form):
+    BANK_CHOICES = (
+        ('ing', 'ING Direct'),
+        # Puedes añadir más bancos en el futuro
+    )
+    
+    bank_provider = forms.ChoiceField(choices=BANK_CHOICES, label="Proveedor bancario")
+    account = forms.ModelChoiceField(queryset=Account.objects.all(), label="Cuenta destino")
+    file = forms.FileField(label="Archivo CSV")
+
 TransactionFormSet = inlineformset_factory(Entry, Transaction, form=TransactionForm, extra=1)
--- registros/urls.py
+++ registros/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
-from .views import account_tree_view, edit_account, upload_attachments, add_account, edit_entry, add_entry, upload_entry_attachments, update_transaction, add_transaction
 from . import views
+from .views import BankImportPreviewView
 
 urlpatterns = [
-    path('account_tree/', account_tree_view, name='account_tree'),
-    path('edit_account/<int:account_id>/', edit_account, name='edit_account'),
-    path('upload_attachments/<int:account_id>/', upload_attachments, name='upload_attachments'),
-    path('add_account/', add_account, name='add_account'),
-    path('edit_entry/<int:entry_id>/', edit_entry, name='edit_entry'),
-    path('add_entry/', add_entry, name='add_entry'),
-    path('upload_entry_attachments/<int:entry_id>/', upload_entry_attachments, name='upload_entry_attachments'),
-    path('update_transaction/<int:transaction_id>/', update_transaction, name='update_transaction'),
-    path('add_transaction/', add_transaction, name='add_transaction'),    
+    path('account_tree/', views.account_tree_view, name='account_tree'),
+    path('edit_account/<int:account_id>/', views.edit_account, name='edit_account'),
+    path('upload_attachments/<int:account_id>/', views.upload_attachments, name='upload_attachments'),
+    path('add_account/', views.add_account, name='add_account'),
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+    path('add_entry/', views.add_entry, name='add_entry'),
+    path('upload_entry_attachments/<int:entry_id>/', views.upload_entry_attachments, name='upload_entry_attachments'),
+    path('update_transaction/<int:transaction_id>/', views.update_transaction, name='update_transaction'),
+    path('add_transaction/', views.add_transaction, name='add_transaction'),    
     path('delete_transaction/<int:transaction_id>/', views.delete_transaction, name='delete_transaction'),
     path('update_attachment_description/<int:attachment_id>/', views.update_attachment_description, name='update_attachment_description'),
     path('delete_attachment/<int:attachment_id>/', views.delete_attachment, name='delete_attachment'),
+    path('import_movements/', views.BankImportView.as_view(), name='bank_import'),
+    path('bank_import_preview/', BankImportPreviewView.as_view(), name='bank_import_preview'),
 ]
