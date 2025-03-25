da8f5403cb861775b02e78e48064fa2e32ff58b1
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2025-03-25 00:10:39 +0000

    Solventar bug con acceso a determinadas ediciones

--- accounts/templatetags/account_tags.py
+++ accounts/templatetags/account_tags.py
@@ -13,7 +13,7 @@ def generate_account_tree(accounts):
             html += f'<div class="col-md-6">'
             if account.children.exists():
                 html += f'<span class="caret" onclick="toggleNested(this)"></span> '
-            html += f'<a href="/accounts/edit_account/{ account.id }">{account.name}</a></div>'
+            html += f'<a href="/accounts/edit_account/{ account.id }/">{account.name}</a></div>'
             html += f'<div class="col-md-6">{account.get_balance()}</div>'
             html += f'</div>'
             if account.children.exists():
