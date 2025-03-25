6a178b438445b859bc1902c3f7f47d8df55ce7bc
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2025-03-25 00:04:30 +0000

    Añadir logo y relleno de index

--- GARCA/settings.py
+++ GARCA/settings.py
@@ -125,13 +125,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'GARCA', 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/attachments/'
 MEDIA_ROOT = BASE_DIR 
 ADMIN_SITE_TITLE = 'GARCA'
 ADMIN_SITE_HEADER = 'GARCA'
