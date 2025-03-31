bfbaff8efbaaab8f6316fcbfc78dffa1e92ba245
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2025-03-31 06:45:56 +0100

    Añadir formato cuenta en importación bancaria

--- bank_imports/forms.py
+++ bank_imports/forms.py
@@ -4,9 +4,16 @@ from accounts.models import Account
 class BankImportForm(forms.Form):
     BANK_CHOICES = (
         ('ing', 'ING Direct'),
-        # Puedes añadir más bancos en el futuro
     )
     
     bank_provider = forms.ChoiceField(choices=BANK_CHOICES, label="Proveedor bancario")
-    account = forms.ModelChoiceField(queryset=Account.objects.all(), label="Cuenta destino")
+    account = forms.ModelChoiceField(
+        queryset=Account.objects.all().order_by('name'),
+        label="Cuenta destino"
+    )
     file = forms.FileField(label="Archivo CSV")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Configurar el label_from_instance para mostrar la jerarquía completa
+        self.fields['account'].label_from_instance = lambda obj: obj.get_full_hierarchy()
