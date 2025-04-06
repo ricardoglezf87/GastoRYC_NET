70adca42fdb10896ffb12d44bd34b1e013e3c808
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2025-04-06 11:10:28 +0100

    Crear botón de recategorizar

--- reports/urls.py
+++ reports/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('unbalanced_entries/', views.unbalanced_entries_report, name='unbalanced_entries_report'),
+    path('recategorized_entries/', views.recategorized_entries, name='recategorized_entries'),
 ]
