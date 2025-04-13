dda5934e28a2fbdf9cf82b9e9593030ddb8e23c6
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2025-04-14 00:30:31 +0100

    arreglo script

--- scripts/iniciar_servidor.sh
+++ scripts/iniciar_servidor.sh
@@ -9,7 +9,7 @@ PID_FILE="/volume1/web/GastoRYC_NET/logs/django_server.pid"
 cd "$DJANGO_DIR" || exit 1
 
 # Ejecutar el servidor Django en segundo plano y guardar PID
-nohup python3.9 -m manage runsslserver 0.0.0.0:8585 > "$LOG_FILE" 2>&1 &
+nohup python3.9 -m manage runserver 0.0.0.0:8585 > "$LOG_FILE" 2>&1 &
 
 # Guardar el PID del proceso
 echo $! > "$PID_FILE"
